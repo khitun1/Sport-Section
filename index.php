fb82383b9e5f664c5937d8ef071477e8ea25d6c3
Admin <khitun1>
2022-02-14 10:10:05 +0500

    new

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     }
 
     try {
-        $conn = new PDO("mysql:host=$_ENV[dbhost];dbname=$_ENV[dbname]", "$_ENV[dbuser]", "$_ENV[dbpassword]");
+        $conn = new PDO("mysql:host=$_ENV[dbhost];dbname=$_ENV[dbname]", $_ENV['dbuser'], $_ENV['dbpassword']);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully";
